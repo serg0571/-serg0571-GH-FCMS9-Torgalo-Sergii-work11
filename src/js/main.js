6600be634cf6589d026f7c130701d04c914a40d2
Sergii Torgalo <serg.t0071@gmail.com>
2020-01-10 21:04:27 +0200

    my12taskS 21;00 fridy

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,41 @@
-let point;
+let point
 $(document).ready(function () {
 
 })
 $('#click-me').on('click', function (event) {
-  event.preventDefault();
+  event.preventDefault()
 
   let text2 = $('#name').val();
   $('.list').append('<li > <span class="my-change">' + text2 + '</span><button type="button" ' +
     'class="btn btn-primary my-btn edit">edit</button>' +
-    '<button type="button" class="btn btn-primary my-btn delete" >delete</button></li>');
+    '<button type="button" class="btn btn-primary my-btn delete" >delete</button></li>')
 
 
-  $('#name').val('');
-  // console.log($('#name').val());
+  $('#name').val('')
+  // console.log($('#name').val())
 
   $('.delete').on('click', function (event) {
     $(event.target.parentElement).remove()
   });
 
   $('.edit').on('click', function (event) {
-    event.preventDefault();
-    let edit = event.target.parentElement.children[0].innerHTML;
-    point = event.target.parentElement.children[0];
-    // console.log(edit);
+    event.preventDefault()
+    let edit = event.target.parentElement.children[0].innerHTML
+    point = event.target.parentElement.children[0]
+    // console.log(edit)
 
-    $('#change').css('display', 'block');
-    $('.edit-form').val(edit);
+    $('#change').css('display', 'block')
+    $('.edit-form').val(edit)
 
-  });
-  $('.change-btn1').on('click', function (event) {
-    event.preventDefault();
-    let editForm = $('.edit-form').val();
-    $(point).html(editForm)
-    $('#change').css('display', 'none');
   })
-});
+
+  $('.change-btn1').on('click', function (event) {
+    event.preventDefault()
+    let editForm = $('.edit-form').val()
+    $(point).html(editForm)
+    $('#change').css('display', 'none')
+  })
+})
 
 
 
